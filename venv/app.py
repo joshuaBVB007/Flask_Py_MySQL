4c7ee0ef03ae3786db7f84e08b1d93fc123fad1e
Jonathan Calderon <jaksvafa7@gmail.com>
2022-11-24 14:12:38 +0100

    CRUD validated

--- venv/app.py
+++ venv/app.py
@@ -18,31 +18,31 @@ def select():
     cursor_BD.close()
     return f"<p>{article}</p>"    
 
-@app.route('/insert')
-def insert():
+@app.route('/insert/<name>')
+def insert(name):
     cursor_BD=my.connection.cursor()
     #Executing SQL Statements
-    cursor_BD.execute(''' INSERT INTO profile VALUES(%s,%s)''',(0,"fifa23"))
+    cursor_BD.execute(''' INSERT INTO profile VALUES(%s,%s)''',(0,{name}))
     my.connection.commit()
     #Closing the cursor
     cursor_BD.close()
     return f"<p>data has been inserted correctly</p>"
 
-@app.route('/update')
-def update():
+@app.route('/update/<new>/<old>')
+def update(new,old):
     cursor_BD=my.connection.cursor()
     #Executing SQL Statements
-    cursor_BD.execute(''' update profile set bio=%s where bio=%s ''',("Ignacio","maria"))
+    cursor_BD.execute(''' update profile set bio=%s where bio=%s ''',({new},{old}))
     my.connection.commit()
     #Closing the cursor
     cursor_BD.close()
     return f"<p>update has been done correctly</p>"
 
-@app.route('/delete')
-def delete():
+@app.route('/delete/<number>')
+def delete(number):
     cursor_BD=my.connection.cursor()
     #Executing SQL Statements
-    cursor_BD.execute(''' delete from profile where id=%s''',("6"))
+    cursor_BD.execute(''' delete from profile where id=%s''',({number}))
     my.connection.commit()
     #Closing the cursor
     cursor_BD.close()
